316c42963c1c8db72cdcadcc514102466f3c298e
Binti016 <anti.binti@gmail.com>
2024-03-21 03:27:53 +0600

    hudai

--- src/index.js
+++ src/index.js
@@ -15,7 +15,6 @@ import {
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-      {/* <Navbar/> */}
     <BrowserRouter>
     <Routes>
       <Route>
